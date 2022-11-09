7d64217fe8b90bb8dcc9b564fbcb88ec4773a646
vluz <vluz@users.noreply.github.com>
2022-11-09 11:34:56 +0000

    First few bits put in, still looks awful

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -11,6 +11,10 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Font Awesome 6 Brands-Regular-400.otf", "FontBrands");
+				fonts.AddFont("Font Awesome 6 Free-Regular-400.otf", "FontRegular");
+				fonts.AddFont("Font Awesome 6 Free-Solid-900.otf", "FontSolid");
+				fonts.AddFont("simplifica.ttf", "FontSimplifica");
 			});
 
 		return builder.Build();
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CVasApp
 CV as an App demonstration
+Intended as demonstration of .net MAUI
