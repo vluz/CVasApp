6c1206464da15965c7c71fe2df07ff7aadef449c
vluz <vluz@users.noreply.github.com>
2022-11-09 18:58:29 +0000

    Look and feel a bit better

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,13 +1,17 @@
 ﻿namespace CVasApp;
+using CommunityToolkit.Maui;
 
 public static class MauiProgram
 {
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+
+		builder.UseMauiApp<App>();
+
+            builder.UseMauiCommunityToolkit();
+			
+			builder.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
@@ -15,7 +19,11 @@ public static class MauiProgram
 				fonts.AddFont("Font Awesome 6 Free-Regular-400.otf", "FontRegular");
 				fonts.AddFont("Font Awesome 6 Free-Solid-900.otf", "FontSolid");
 				fonts.AddFont("simplifica.ttf", "FontSimplifica");
-			});
+                //fonts.AddFont("", "Font");
+                fonts.AddFont("CooperHewitt-Light.otf", "FontCHLight");
+                fonts.AddFont("CooperHewitt-Medium.otf", "FontCHMedium");
+                fonts.AddFont("CooperHewitt-Semibold.otf", "FontCHBold");
+            });
 
 		return builder.Build();
 	}
